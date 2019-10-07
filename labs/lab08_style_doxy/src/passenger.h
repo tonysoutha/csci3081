4979d95d004e02a8e0ebc82742a111ea805c81b0
Tony Southa <south211@umn.edu>
2019-10-07 17:06:34 -0500

    lab08

--- labs/lab08_style_doxy/src/passenger.h
+++ labs/lab08_style_doxy/src/passenger.h
@@ -4,13 +4,24 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief The class for Passengers.
+ */
 class Passenger { // : public Reporter {
  public:
+/**
+ * @param[in] first parameter takes in an int for the time for the passenger
+   @param[in] second parameter takes in a string of the passenger's name
+ */
+
   Passenger(int = -1, std::string = "Nobody");
   void Update();
   void GetOnBus();
   int GetTotalWait() const;
   bool IsOnBus() const;
+  /**
+   * The function GetDestination will return the int destination_stop_id
+   */
   int GetDestination() const;
   void Report() const;
  private:
@@ -20,6 +31,6 @@ class Passenger { // : public Reporter {
   int time_on_bus_;
   int id_;
   static int count_;  // global count, used to set ID for new instances
-  
+
 };
 #endif
