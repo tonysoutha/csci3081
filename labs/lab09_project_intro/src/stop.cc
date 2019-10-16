b1d50570f12f621b6c3406a45be0f9658434f0dd
Tony Southa <south211@umn.edu>
2019-10-16 16:53:43 -0500

    lab09

--- labs/lab09_project_intro/src/stop.cc
+++ labs/lab09_project_intro/src/stop.cc
@@ -1,32 +1,40 @@
+// Copyright 2019 Tony Southa
+
 #include <iostream>
 #include <vector>
-	
-#include "stop.h"
-	
-Stop::Stop(int id, double longitude, double latitude) { //Defaults to Westbound Coffman Union stop
-	  // no initialization of list of passengers necessary
+
+#include "src/stop.h"
+
+Stop::Stop(int id, double longitude, double latitude) {
+    // Defaults to Westbound Coffman Union stop
+    // no initialization of list of passengers necessary
+    id_ = id;
+    longitude_ = longitude;
+    latitude_ = latitude;
 }
-	
 
 void Stop::LoadPassengers(Bus * bus) {
-	  //loading some passengers onto a bus
+    // loading some passengers onto a bus
+    for (std::list<Passenger *>::iterator it = passengers_.begin();
+        it != passengers_.end(); it++) {
+        bus.LoadPassenger(*it);
+    }
 }
-	
 
 void Stop::AddPassengers(Passenger * pass) {
-	  //add the passenger to the stop
+    // add the passenger to the stop
+    passengers_.push_back(pass);
 }
-	
 
-int Stop::GetId() const{
-	  return id_;
+int Stop::GetId() const {
+    return id_;
 }
-	
 
 void Stop::Report() const {
-	  std::cout << "ID: " << id_ << std::endl;
-	  std::cout << "Passengers waiting: " << passengers_.size() << std::endl;
-	  for(std::list<Passenger *>::const_iterator it = passengers_.begin(); it != passengers_.end(); it++) {
-	    (*it)->Report();
-	  }
+    std::cout << "ID: " << id_ << std::endl;
+    std::cout << "Passengers waiting: " << passengers_.size() << std::endl;
+    for (std::list<Passenger *>::const_iterator it = passengers_.begin();\
+    it != passengers_.end(); it++) {
+        (*it)->Report();
+    }
 }
