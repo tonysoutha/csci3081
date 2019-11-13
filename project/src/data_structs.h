e59825a27b3c3cdc92ae2bd54444403d93d165bf
Danielwbolson <danielwbolson@gmail.com>
2019-11-13 17:36:36 -0600

    Adding visualization code
    - Headstart on makefile
    - Visualization_simulator and config_manager
    - Actual web graphics code
    - UML for visualization
    - new config directory and a default config.txt

---
+++ project/src/data_structs.h
@@ -0,0 +1,36 @@
+
+#ifndef DATA_STRUCTS_H_
+#define DATA_STRUCTS_H_
+
+#include <string>
+#include <vector>
+
+
+struct Position {
+    Position() : x(0), y(0) {}
+    float x;
+    float y;
+};
+
+struct BusData {
+    BusData() : id(""), position(Position()), num_passengers(0), capacity(0) {}
+    std::string id; // some unique identifier for the bus
+    Position position; // long lat position of bus
+    int num_passengers; 
+    int capacity;
+};
+
+struct StopData {
+    StopData() : id(""), position(Position()), num_people(0) {}
+    std::string id; // some unique identifier for the stop
+    Position position; // long lat position of bus
+    int num_people;
+};
+
+struct RouteData {
+    RouteData() : id(""), stops(std::vector<StopData>(0)) {}
+    std::string id; // some unique identifier for the route
+    std::vector<StopData> stops;
+};
+
+#endif // DATA_STRUCTS_H_
