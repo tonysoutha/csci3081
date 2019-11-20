727c99efdfc7bec9d2a9240589f37b87333776d7
Tony Southa <south211@umn.edu>
2019-11-19 19:42:16 -0600

    fix(new_local_simulator): fix #11 implemented class to send output to a file fix(capture_transit_sim): fix #11 implemented driver to create ofstream object to write to a file

---
+++ project/src/new_local_simulator.h
@@ -0,0 +1,27 @@
+/**
+ * @file new_local_simulator.h
+ *
+ * @copyright 2019 3081 Staff, All rights reserved.
+ */
+#ifndef SRC_NEW_LOCAL_SIMULATOR_H_
+#define SRC_NEW_LOCAL_SIMULATOR_H_
+
+#include <vector>
+
+#include "src/simulator.h"
+#include <fstream>
+
+class NewLocalSimulator : public Simulator {
+ public:
+  bool Start() override;
+  bool Update() override;
+  void SetOutput(std::ostream * f);
+ private:
+  std::vector<int> bus_counters_;
+  std::vector<int> bus_start_timings_;
+  std::vector<int> time_since_last_bus_generation_;
+  int simulation_time_elapsed_;
+  std::ostream * f_ptr;
+};
+
+#endif  // SRC_NEW_LOCAL_SIMULATOR_H_
